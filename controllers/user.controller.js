6bc996860dd9e9c96f0611aa2c9a400ea3fa1b47
Lukas <mikaur00@gmail.com>
2023-05-12 19:03:56 +0200

    metodos login&register Route::post('login', 'Auth\LoginController@login');

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -9,60 +9,6 @@ const { secretKey, expiredAfter } = authConfig;
 const controller = express.Router();
 
 // vinculamos la ruta /api/usuarios a la función declarada
-// GET lista de todos los usuarios
-controller.get("/", function (req, res, next) {
-
-    sequelize.sync().then(() => {
-
-        Usuario.findAll({
-            include: {
-                model: Objeto
-            }
-        })
-            //.then(alumnes => res.json(alumnes))
-            .then(usuarios => res.json({
-                ok: true,
-                data: usuarios
-            }))
-            .catch(error => res.json({
-                ok: false,
-                error: error
-            }))
-
-    }).catch((error) => {
-        res.json({
-            ok: false,
-            error: error
-        })
-    });
-
-});
-// GET busca unico usuario
-controller.get('/:id', function (req, res, next) {
-    sequelize.sync().then(() => {
-
-        Usuario.findOne({
-            where: { id: req.params.id },
-            include: {
-                model: Objeto
-            }
-        })
-            .then(al => res.json({
-                ok: true,
-                data: al
-            }))
-            .catch(error => res.json({
-                ok: false,
-                error: error
-            }))
-
-    }).catch((error) => {
-        res.json({
-            ok: false,
-            error: error
-        })
-    });
-});
 
 // POST Login, busca usuario en BDD para corelar los datos
 controller.post('/login', async (req, res) => {
@@ -121,51 +67,5 @@ controller.post('/register', (req, res, next) => {
         })
     });
 });
-
-// PUT actualiza datos usuario
-controller.put('/:id', function (req, res, next) {
-    sequelize.sync().then(() => {
-
-        Usuario.findOne({ where: { id: req.params.id } })
-            .then(usuario_trobat =>
-                usuario_trobat.update(req.body)
-            )
-            .then(usuario_modificat => res.json({
-                ok: true,
-                data: usuario_modificat
-            }))
-            .catch(error => res.json({
-                ok: false,
-                error: error.message
-            }));
-
-    }).catch((error) => {
-        res.json({
-            ok: false,
-            error: error.message
-        })
-    });
-});
-
-
-
-// DELETE elimina usuario
-controller.delete('/:id', function (req, res, next) {
-
-    sequelize.sync().then(() => {
-
-        Usuario.destroy({ where: { id: req.params.id } })
-            .then((data) => res.json({ ok: true, data }))
-            .catch((error) => res.json({ ok: false, error }))
-
-    }).catch((error) => {
-        res.json({
-            ok: false,
-            error: error
-        })
-    });
-
-});
-
 
 export default controller;
